f2dbf49d90bb87b5e2c243a65170c9aa1c364e8b
Edan Bash <edansbash@gmail.com>
2018-08-12 21:12:53 -0800

    Added buffet car labs

--- 42 - @Make 100 recycled cars/starter.java
+++ 42 - @Make 100 recycled cars/starter.java
@@ -1,56 +1,60 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.*;
+import java.awt.event.*;
 
 public class starter extends JPanel implements Runnable
 {
+	//private variables
 	static JFrame frame;
 	
 	int CAR_WIDTH=60;
 	Car[] cars;
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		frame = new JFrame("App");
 		frame.add(new starter());
 		frame.setSize(600,600);
 		frame.setVisible(true);
 	}
-	
+	//creates all objects
 	public starter()
-	{
+	{	
 		Color a = new Color(0,255,0);
 		setBackground(a);
 		
-		String[] names = new String[]{"BOB","SUE","JOE"};
-		Color[] colors = new Color[]{Color.BLUE,Color.RED,Color.YELLOW};
+		cars = new Car[100];
+		makeCars();
 		
-		//this amount of cars will not overlap when recycled
-		cars = new Car[6];
-		for(int i=0;i<cars.length;i++)
+		Thread thread = new Thread(this);
+		//calls the run function
+		thread.start();
+	}
+	public void makeCars()
+	{
+		String[] names = new String[]{"BOB","SUE","JOE","EDAN","RICKY","NEAT"};
+		Color[] colors = new Color[]{Color.BLUE,Color.RED,Color.YELLOW,Color.PINK,Color.MAGENTA,Color.ORANGE};
+		
+		for(int i=0; i<cars.length; i++)
 		{
 			int y = (int)(Math.random()*500);
-			String name = names[i%3];
-			Color color = colors[i%3];
+			String name = names[i%6];
+			Color color = colors[i%6];
 			
 			//makes sure cars start with car width gap between them
 			cars[i] = new Car(-i*2*CAR_WIDTH,y,name,color,2);
 		}
-		
-		Thread thread = new Thread(this);
-		thread.start();//calls the run function
 	}
 	
+	//called by repaint
 	public void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
 		
-		for(int i=0;i<cars.length;i++)
-		{
+		for(int i=0; i<cars.length; i++)
 			cars[i].paint(g);
-		}
-
 	}
-	
 	
 	public void run()
 	{
@@ -58,22 +62,17 @@ public class starter extends JPanel implements Runnable
 		{
 			for(int i=0;i<cars.length;i++)
 			{
-				if(cars[i].getX()>=600)
+				//recycles cars if they go off screen and makes sure no car overlap
+				if(cars[i].getX() >= 600)
 				{
-					//make car spawn at new location
-					int y = (int)(Math.random()*500);
-					
-					//makes sure cars continue with car width gap between them
-					cars[i].setLocation(-2*CAR_WIDTH,y);
+					cars[i].setLocation(-180*CAR_WIDTH,(int)(Math.random()*500));
 				}
 				cars[i].drive();
 			}
 			repaint();
-			
 		
 			try { Thread.sleep(15); }
 			catch (InterruptedException e) { }
 		}
 	}
 }
-
---
+++ 43 - (new)Make a Player class/starter.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.*;
+import java.awt.event.*;
+
+public class starter extends JPanel
+{
+	//private variables
+	static JFrame frame;
+	Player p;
+
+	public static void main(String[] args)
+	{
+		frame = new JFrame("App");
+		frame.add(new starter());
+		frame.setSize(600,600);
+		frame.setVisible(true);
+	}
+	//creates all objects
+	public starter()
+	{	
+		Color a = new Color(0,255,0);
+		setBackground(a);
+	
+		p = new Player(50,50);
+	}
+	
+	//called by repaint
+	public void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D)g;
+		p.paint(g2);
+	}
+	
+	
+
+}
