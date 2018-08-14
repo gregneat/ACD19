975fff50fa1ba0c9c3c7aeb6e6834a83b7541e6b
Edan Bash <edansbash@gmail.com>
2018-08-14 15:33:29 -0800

    fully updated labs

--- File Name Manager/RenameTester.java
+++ File Name Manager/RenameTester.java
@@ -10,7 +10,7 @@ public class RenameTester
 		String folder_path = "C:\\Users\\edans\\Documents\\GitHub\\ACD19";
         // creating new folder
         File myfolder = new File(folder_path);
-		//Renamer.shiftFileOrder(myfolder,48,69);
+		//Renamer.shiftFileOrder(myfolder,101,109);
 		Renamer.renameFilesInBinary(myfolder);
 		//Renamer.renameFilesInBase10(myfolder);
 		System.out.println("Files have been changed");
